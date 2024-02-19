4abf5710a99ccbd27ee0d4dea9bb28681c165137
Rog3rSm1th <r0g3r5@protonmail.com>
2024-02-19 23:21:00 +0100

    Add unit tests for the `from_pem` method

--- okamoto-uchiyama-rs/tests/test_pem.rs
+++ okamoto-uchiyama-rs/tests/test_pem.rs
@@ -48,3 +48,48 @@ fn test_private_key_pem_encoding() {
     // Assert equality
     assert_eq!(pem_str, expected_pem);
 }
+
+#[test]
+fn test_parse_public_key_from_pem() {
+    // Define the PEM-encoded public key string
+    let pem_str = "-----BEGIN PUBLIC KEY-----\n\
+                    MBUCBQIyNHTHAgUB4dOT9wIFAdwgA/E=\n\
+                    -----END PUBLIC KEY-----\n";
+
+    // Parse the PEM-encoded string into a PublicKey instance
+    let parsed_public_key = PublicKey::from_pem(pem_str).unwrap();
+
+    let expected_public_key = PublicKey::new(
+        &BigUint::from(9432233159u64),
+        &BigUint::from(8083706871u64),
+        &BigUint::from(7988052977u64),
+    );
+
+    // // Assert equality between the parsed and expected public keys
+    assert_eq!(parsed_public_key, expected_public_key);
+}
+
+#[test]
+fn test_parse_private_key_from_pem() {
+    // Define the PEM-encoded private key string
+    let pem_str = "-----BEGIN PRIVATE KEY-----\n\
+                    MCcCBQIyNHTHAgUB4dOT9wIFAdwgA/ECAx9jegICB9MCAgkvAgM9N+k=\n\
+                    -----END PRIVATE KEY-----\n";
+
+    // Parse the PEM-encoded string into a PrivateKey instance
+    let parsed_private_key = PrivateKey::from_pem(pem_str).unwrap();
+
+    let public_key = PublicKey::new(
+        &BigUint::from(9432233159u64),
+        &BigUint::from(8083706871u64),
+        &BigUint::from(7988052977u64),
+    );
+    let expected_private_key = PrivateKey::new(
+        &public_key,
+        &BigUint::from(2003u64),
+        &BigUint::from(2351u64),
+    );
+
+    // Assert equality between the parsed and expected private keys
+    assert_eq!(parsed_private_key, expected_private_key);
+}
